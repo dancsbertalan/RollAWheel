650862d3f6021e46e1c1acd080c3b08fe84d1156
AndorSebo <andorsebo@gmail.com>
2016-10-10 18:19:06 +0200

    Teszt pálya
    Az első teszt pályát elhelyeztem, a kamera még nem működik.

---
+++ Forraskod/RAW/Assets/_Szkriptek/_kameraKerek.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class _kameraKerek : MonoBehaviour
+{
+    public Transform Player;
+    Vector3 Direction;
+    void Start()
+    {
+        Direction = transform.position - Player.transform.position;
+    }
+    void Update()
+    {      
+            transform.position = Player.transform.position + Direction;
+        
+    }
+
+}
