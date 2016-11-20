573e9395c2b8203cb2c0bd1dd51de1db1c33147b
AndorSebo <andorsebo@gmail.com>
2016-11-20 13:19:19 +0100

    Irányítás script, most már komolyan
    Az előző feltöltés során valamiért nem írta felül az irányítás scriptet,
    most újra feltöltésre kerül.

--- Forraskod/RAW/Assets/_Szkriptek/_kerekMozgas.cs
+++ Forraskod/RAW/Assets/_Szkriptek/_kerekMozgas.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class _kerekMozgas : MonoBehaviour
 {
     bool jobb, bal = false;
+    public Button Jobbra, Balra;
+
+    void Start()
+    {
+        if (!(Application.platform == RuntimePlatform.Android))
+        {        
+            Jobbra.enabled = false;
+            Jobbra.GetComponentInChildren<CanvasRenderer>().SetAlpha(0);
+            Jobbra.GetComponentInChildren<Text>().color = Color.clear;
+            Balra.enabled = false;
+            Balra.GetComponentInChildren<CanvasRenderer>().SetAlpha(0);
+            Balra.GetComponentInChildren<Text>().color = Color.clear;
+        }
+    }
 
     void Update()
     {
@@ -14,12 +28,14 @@ public class _kerekMozgas : MonoBehaviour
     }
     public void Jobb(GameObject Player)
     {
-        jobb = true;
+        if (Application.platform == RuntimePlatform.Android)
+            jobb = true;
         Player.transform.GetComponent<Rigidbody2D>().AddForce(transform.position.x * new Vector2(0.1f, 0));
     }
     public void Bal(GameObject Player)
     {
-        bal = true;
+        if (Application.platform == RuntimePlatform.Android)
+            bal = true;
         Player.transform.GetComponent<Rigidbody2D>().AddForce(transform.position.x * new Vector2(-0.1f, 0));
     }
     public void felenged()
